dc96c343021f0310ce379f61ad691cc8fe132452
walterpreye <walterpreye@gmail.com>
2026-02-09 01:35:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ const countdown = setInterval(() => {
   document.getElementById("countdown").innerHTML =
     `${days} Days • ${hours} Hours • ${minutes} Minutes • ${seconds} Seconds`;
 }, 1000);
+
+const animatedSections = document.querySelectorAll('.animate');
+
+const revealOnScroll = () => {
+  const triggerBottom = window.innerHeight * 0.85;
+
+  animatedSections.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+
+    if (sectionTop < triggerBottom) {
+      section.classList.add('show');
+    }
+  });
+};
+
+window.addEventListener('scroll', revealOnScroll);
+revealOnScroll();
